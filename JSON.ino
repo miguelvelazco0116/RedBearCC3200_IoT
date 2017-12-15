d4d814250013cc4f9a54b4c909b5d6dd844b4486
MikeVelazcoOrihuela <miguel.angel.velazco.mav@gmail.com>
2017-12-14 21:20:32 -0600

    Add files via upload

---
+++ JSON.ino
@@ -0,0 +1,26 @@
+// ************************************************************************************************************************
+// ************************************************************************************************************************
+
+void createJSON(float data00, char* source00, float data01, char* source01)
+{  
+  float data2send00, data2send01;  
+  
+  data2send00 = data00;
+  data2send01 = data01;
+  aJsonObject *dataJson;
+
+  dataJson = aJson.createObject();
+  aJson.addItemToObject(dataJson, source00, aJson.createItem(data2send00));
+  aJson.addItemToObject(dataJson, source01, aJson.createItem(data2send01));
+   
+  char* JSON = aJson.print(dataJson);  
+  client.publish(topic_MQTT, JSON);  
+
+  aJson.deleteItem(dataJson);  
+  dataJson = aJson.parse(JSON);
+  free(JSON);
+  JSON = aJson.print(dataJson);
+  free(JSON);
+  aJson.deleteItem(dataJson);  
+  
+}
